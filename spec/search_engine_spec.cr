73c1eb110df9d592533be667ccad10eb3d03dcf0
Saghm Rossi <saghmrossi@gmail.com>
2016-02-06 17:20:41 -0500

    add more normalization tests and move to own spec file

--- spec/search_engine_spec.cr
+++ spec/search_engine_spec.cr
@@ -1,19 +1,4 @@
 require "./spec_helper"
 
 describe SearchEngine do
-  it "normalize simple" do
-    norm   = Normalization::Normalizer.new([] of String, "", true)
-    text   = "The rat the cat the dog chased killed ate the malt, dude."
-    output = %w(The rat the cat the dog chased killed ate the malt, dude.)
-
-    norm.normalize(text).should eq(output)
-  end
-
-  it "normalize punctation" do
-    norm   = Normalization::Normalizer.new([] of String, ".,", true)
-    text   = "The rat the cat the dog chased killed ate the malt, dude."
-    output = %w(The rat the cat the dog chased killed ate the malt dude)
-
-    norm.normalize(text).should eq(output)
-  end
 end
--- src/search_engine/normalizer.cr
+++ src/search_engine/normalizer.cr
@@ -1,28 +1,30 @@
-module Normalization
-  class Normalizer
-    def initialize(@stopwords, @punctation, @ignore_quotes)
-    end
-
-    def normalize(text, stopwords = @stopwords, punctation = @punctation)
-      words = text.split(get_splitter_regex(punctation))
-
-      words.reduce([] of String) do |normalized_words, word|
-        word = strip_single_quotes(word) if @ignore_quotes
-
-        normalized_words << word unless @stopwords.includes?(word) || word.empty?
-        normalized_words
+module SearchEngine
+  module Normalization
+    class Normalizer
+      def initialize(@stopwords, @punctation, @ignore_quotes)
       end
-    end
 
-    private def get_splitter_regex(string)
-      char_strings = string.split("").reject(&.empty?)
-      char_regexes = char_strings.map { |c| Regex.new(Regex.escape(c)) }
+      def normalize(text, stopwords = @stopwords, punctation = @punctation)
+        words = text.downcase.split(get_splitter_regex(punctation))
 
-      Regex.union([/\s+/] + char_regexes)
-    end
+        words.reduce([] of String) do |normalized_words, word|
+          word = strip_single_quotes(word) if @ignore_quotes
 
-    private def strip_single_quotes(word)
-      word.chomp('\'').sub(/^'+/, "")
+          normalized_words << word unless @stopwords.includes?(word) || word.empty?
+          normalized_words
+        end
+      end
+
+      private def get_splitter_regex(string)
+        char_strings = string.split("").reject(&.empty?)
+        char_regexes = char_strings.map { |c| Regex.new(Regex.escape(c)) }
+
+        Regex.union([/\s+/] + char_regexes)
+      end
+
+      private def strip_single_quotes(word)
+        word.chomp('\'').sub(/^'+/, "")
+      end
     end
   end
 end
