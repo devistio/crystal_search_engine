a56e95892b10c51f6b9a19f9232382e4cf75e446
Saghm Rossi <saghmrossi@gmail.com>
2016-02-06 00:23:34 -0500

    tests for basic text normalization

--- spec/search_engine_spec.cr
+++ spec/search_engine_spec.cr
@@ -1,9 +1,19 @@
 require "./spec_helper"
 
 describe SearchEngine do
-  # TODO: Write tests
+  it "normalize simple" do
+    norm   = Normalization::Normalizer.new([] of String, "", true)
+    text   = "The rat the cat the dog chased killed ate the malt, dude."
+    output = %w(The rat the cat the dog chased killed ate the malt, dude.)
 
-  it "works" do
-    false.should eq(true)
+    norm.normalize(text).should eq(output)
+  end
+
+  it "normalize punctation" do
+    norm   = Normalization::Normalizer.new([] of String, ".,", true)
+    text   = "The rat the cat the dog chased killed ate the malt, dude."
+    output = %w(The rat the cat the dog chased killed ate the malt dude)
+
+    norm.normalize(text).should eq(output)
   end
 end
