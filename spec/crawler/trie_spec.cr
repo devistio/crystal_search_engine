685936df7c441cf64eaefed6c5b07cffad75e0fb
Saghm Rossi <saghmrossi@gmail.com>
2016-02-07 23:37:10 -0500

    revise Trie to store String => Int32 counts instead of singular count

--- spec/crawler/trie_spec.cr
+++ spec/crawler/trie_spec.cr
@@ -7,42 +7,77 @@ describe Trie do
     trie = Trie.new
 
     trie.get_count("").should eq(0)
+    trie.get_count("", "alpha").should eq(0)
     trie.get_count("h").should eq(0)
+    trie.get_count("h", "alpha").should eq(0)
+    trie.get_count("you").should eq(0)
+    trie.get_count("you", "alpha").should eq(0)
   end
 
   it "single word" do
     trie = Trie.new
-    trie.insert("hey")
+    trie.insert("hey", "alpha")
 
     trie.get_count("").should eq(0)
+    trie.get_count("", "alpha").should eq(0)
     trie.get_count("h").should eq(0)
+    trie.get_count("h", "alpha").should eq(0)
+    trie.get_count("you").should eq(0)
+    trie.get_count("you", "alpha").should eq(0)
+
     trie.get_count("hey").should eq(1)
+    trie.get_count("hey", "alpha").should eq(1)
   end
 
-  it "same word twice" do
+  it "same word multiple times" do
     trie = Trie.new
-    trie.insert("hey")
-    trie.insert("hey")
+    trie.insert("hey", "alpha")
+    trie.insert("hey", "alpha")
+    trie.insert("hey", "beta")
 
     trie.get_count("").should eq(0)
+    trie.get_count("", "alpha").should eq(0)
+    trie.get_count("", "beta").should eq(0)
     trie.get_count("h").should eq(0)
-    trie.get_count("hey").should eq(2)
+    trie.get_count("h", "alpha").should eq(0)
+    trie.get_count("h", "beta").should eq(0)
+    trie.get_count("you").should eq(0)
+    trie.get_count("you", "alpha").should eq(0)
+    trie.get_count("you", "beta").should eq(0)
+
+    trie.get_count("hey").should eq(3)
+    trie.get_count("hey", "alpha").should eq(2)
+    trie.get_count("hey", "beta").should eq(1)
   end
 
   it "many words" do
     trie = Trie.new
-    trie.insert("hey")
-    trie.insert("hey")
-    trie.insert("hay")
-    trie.insert("heaven")
-    trie.insert("ball")
+    trie.insert("hey", "alpha")
+    trie.insert("hey", "alpha")
+    trie.insert("hey", "beta")
+    trie.insert("heaven", "alpha")
+    trie.insert("ball", "beta")
 
     trie.get_count("").should eq(0)
+    trie.get_count("", "alpha").should eq(0)
+    trie.get_count("", "beta").should eq(0)
     trie.get_count("h").should eq(0)
-    trie.get_count("hey").should eq(2)
-    trie.get_count("hay").should eq(1)
+    trie.get_count("h", "alpha").should eq(0)
+    trie.get_count("h", "beta").should eq(0)
+    trie.get_count("you").should eq(0)
+    trie.get_count("you", "alpha").should eq(0)
+    trie.get_count("you", "beta").should eq(0)
+
+    trie.get_count("hey").should eq(3)
+    trie.get_count("hey", "alpha").should eq(2)
+    trie.get_count("hey", "beta").should eq(1)
+
     trie.get_count("heaven").should eq(1)
+    trie.get_count("heaven", "alpha").should eq(1)
+    trie.get_count("heaven", "beta").should eq(0)
+
     trie.get_count("ball").should eq(1)
-    trie.get_count("balk").should eq(0)
+    trie.get_count("ball", "alpha").should eq(0)
+    trie.get_count("ball", "beta").should eq(1)
   end
 end
--- src/search_engine/crawler/index/trie.cr
+++ src/search_engine/crawler/index/trie.cr
@@ -5,28 +5,37 @@ module SearchEngine
       # (String => Int32) map.
       class Trie
         def initialize
-          @out   = {} of Char => Trie
-          @count = 0
+          @out    = {} of Char => Trie
+          @counts = Hash(String, Int32).new(0)
         end
 
-        # Increments the count of a given word.
-        def insert(word)
-          return @count += 1 if word.empty?
+        # Increments the count of a given word in a given document.
+        def insert(word, document, count = 1)
+          return @counts[document] += count if word.empty?
 
           letter    = word.char_at(0)
           next_node = @out[letter] = Trie.new unless next_node = @out[letter]?
 
-          next_node.insert(word[1..-1])
+          next_node.insert(word[1..-1], document, count)
         end
 
         # Returns the count of a given word.
-        def get_count(word)
-          return @count if word.empty?
+        def get_count(word, document = nil)
+
+          if word.empty?
+            count = if document
+                      @counts[document]
+                    else
+                      @counts.values.sum
+                    end
+
+            return count
+          end
 
           letter = word.char_at(0)
 
           if next_node = @out[letter]?
-            next_node.get_count(word[1..-1])
+            next_node.get_count(word[1..-1], document)
           else
             0
           end
