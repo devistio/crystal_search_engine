35495d0f2cc227caf486d44a3398c169e00f7f7b
Saghm Rossi <saghmrossi@gmail.com>
2016-02-07 22:39:56 -0500

    add initial documentation for Trie

--- src/search_engine/index/trie.cr
+++ src/search_engine/index/trie.cr
@@ -1,11 +1,14 @@
 module SearchEngine
   module Index
+    # Used to keep track of word occurences; essentially a memory-efficient
+    # (String => Int32) map.
     class Trie
       def initialize
         @out   = {} of Char => Trie
         @count = 0
       end
 
+      # Increments the count of a given word.
       def insert(word)
         return @count += 1 if word.empty?
 
@@ -15,6 +18,7 @@ module SearchEngine
         next_node.insert(word[1..-1])
       end
 
+      # Returns the count of a given word.
       def get_count(word)
         return @count if word.empty?
 
